701e270d73a5f0c8fb36688387254e84aa9a56a3
jeremy zeta <jeremy_zeta@yahoo.com>
2025-03-09 03:18:47 +0900

    first commit

---
+++ SpendSmart/Models/ExpenseDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+            
+        }
+    }
+}
